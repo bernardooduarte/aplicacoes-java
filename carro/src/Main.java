5715f2092931f2457d0bb17bf987fea195414a8f
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2024-03-02 11:18:31 -0300

    formatando o main da aplicação carro

--- carro/src/Main.java
+++ carro/src/Main.java
@@ -2,17 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Carro c1 = new Carro(0);
 		c1.potencia = 150;
-		
+
 		Carro c2 = new Carro(0);
 		c2.potencia = 150;
-		
-		if (c1 == c2){
-		System.out.println("Objetos iguais.");
+
+		if (c1 == c2) {
+			System.out.println("Objetos iguais.");
 		} else {
-		System.out.println("Objetos diferentes.");
+			System.out.println("Objetos diferentes.");
 		}
 	}
 
