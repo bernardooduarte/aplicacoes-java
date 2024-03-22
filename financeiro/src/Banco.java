ffbd817ab4ddaaa3c4a57c1df97991c940803be4
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2024-03-22 15:57:41 -0300

    melhorando a classe Banco

--- financeiro/src/Banco.java
+++ financeiro/src/Banco.java
@@ -1,44 +1,27 @@
+import java.util.ArrayList;
 
 public class Banco {
 
-	Conta[] listaContas;
-	int total;
-	int n;
+	ArrayList<Conta> listaContas;
 
-	Banco(int n) {
-		this.listaContas = new Conta[n];
+	Banco(){
+		listaContas = new ArrayList<Conta>();
+	}
+	
+	void adicionaConta(Conta c) {
+		listaContas.add(c);
 	}
 
-	Banco() {
-		this.listaContas = new Conta[10];
-	}
-
-	boolean adicionaConta(Conta c) {
-		for (int i = 0; i < listaContas.length; i++) {
-			if (listaContas[i] == null) {
-				listaContas[i] = c;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	Conta getConta(int n) {
-		for (Conta i : listaContas) {
-			if (i.numero == n && i != null) {
-				return i;
+	Conta getConta(int numeroConta) {
+		for (Conta c : listaContas) {
+			if (c.numero == numeroConta) {
+				return c;
 			}
 		}
 		return null;
 	}
 
 	int getTotalContas() {
-		int totalContas = 0;
-		for (Conta contas : this.listaContas) {
-			if (contas != null) {
-				totalContas++;
-			}
-		}
-		return totalContas;
+		return listaContas.size();
 	}
 }
--- financeiro/src/TestaConta.java
+++ financeiro/src/TestaConta.java
@@ -2,85 +2,33 @@ public class TestaConta {
 
 	public static void main(String[] args) {
 
+		Conta c1 = new Conta(123, "Bernardo", 200);
+		Conta c2 = new Conta(456, "Fábio", 250);
+		Conta c3 = new Conta(789, "Carlos", 300);
+	
 		Banco nubank = new Banco();
-
-		Conta c1 = new Conta("Bernardo Duarte");
-		System.out.println("Número da conta gerado para " + c1.titular + ": " + c1.numero);
+		
 		nubank.adicionaConta(c1);
-
-		Conta c2 = new Conta("Felipe Médice");
-		System.out.println("Número da conta gerado para " + c2.titular + ": " + c2.numero);
 		nubank.adicionaConta(c2);
-
-		Conta c3 = new Conta("Lorran Rezende");
-		System.out.println("Número da conta gerado para " + c3.titular + ": " + c3.numero);
 		nubank.adicionaConta(c3);
-
-		Conta c4 = new Conta("Leonardo César");
-		System.out.println("Número da conta gerado para " + c4.titular + ": " + c4.numero);
-		nubank.adicionaConta(c4);
-
-		Conta c5 = new Conta("Rodrigo Guedes");
-		System.out.println("Número da conta gerado para " + c5.titular + ": " + c5.numero);
-		nubank.adicionaConta(c5);
-
-		Conta c6 = new Conta("Lucas Lima");
-		System.out.println("Número da conta gerado para " + c6.titular + ": " + c6.numero);
-		nubank.adicionaConta(c6);
-
-		System.out.println("Total de contas geradas: " + nubank.getTotalContas());
-
-		Banco bradesco = new Banco(5);
-
-		System.out.println("\n");
-
-		Conta c7 = new Conta("Gabriel Fernandes");
-		System.out.println("Número da conta gerado para " + c7.titular + ": " + c7.numero);
-		bradesco.adicionaConta(c7);
-
-		Conta c8 = new Conta("Célio de Assis");
-		System.out.println("Número da conta gerado para " + c8.titular + ": " + c8.numero);
-		bradesco.adicionaConta(c8);
-
-		Conta c9 = new Conta("Pedro Spíndola");
-		System.out.println("Número da conta gerado para " + c9.titular + ": " + c9.numero);
-		bradesco.adicionaConta(c9);
-
-		Conta c10 = new Conta("Pedro Magalhães");
-		System.out.println("Número da conta gerado para " + c10.titular + ": " + c10.numero);
-		bradesco.adicionaConta(c10);
-
-		Conta c11 = new Conta("Vinícus de Souza");
-		System.out.println("Número da conta gerado para " + c11.titular + ": " + c11.numero);
-		bradesco.adicionaConta(c11);
-
-		Conta c12 = new Conta("Hugo de Souza");
-		System.out.println("Número da conta gerado para " + c12.titular + ": " + c12.numero);
-		bradesco.adicionaConta(c12);
-
-		System.out.println("Total de contas geradas: " + bradesco.getTotalContas());
-
-		Banco caixa = new Banco(3);
-
-		System.out.println("\n");
-
-		Conta c13 = new Conta("Joseh");
-		System.out.println("Número da conta gerado para " + c13.titular + ": " + c13.numero);
-		caixa.adicionaConta(c13);
-
-		Conta c14 = new Conta("Bruno Carvalho");
-		System.out.println("Número da conta gerado para " + c14.titular + ": " + c14.numero);
-		caixa.adicionaConta(c14);
-
-		Conta c15 = new Conta("Lucas Bavuso");
-		System.out.println("Número da conta gerado para " + c15.titular + ": " + c15.numero);
-		caixa.adicionaConta(c15);
-
-		Conta c16 = new Conta("Jair Ferreira");
-		System.out.println("Número da conta gerado para " + c16.titular + ": " + c16.numero);
-		caixa.adicionaConta(c16);
-
-		System.out.println("Total de contas geradas: " + caixa.getTotalContas());
+		
+		System.out.println("Listagem das contas geradas:");
+		for(Conta c : nubank.listaContas) {
+			System.out.println(c.titular + " - " + c.numero);
+		}
+		
+		System.out.println("\nEncontrando conta:");
+		
+		Conta conta = nubank.getConta(456);
+		
+		if(conta != null) {
+			System.out.println("Conta encontrada");
+			System.out.println(conta.numero + " - " + conta.titular);
+		} else {
+			System.out.println("Conta não encontrada");
+		}
+		
+		System.out.println("\nTotal de contas geradas: " + nubank.getTotalContas());
 
 	}
 }
